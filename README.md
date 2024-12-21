8eb7ece45666fb2242c50db93b02a44282478cb5
jonathanwambua <jonathanwambua10219@gmail.com>
2024-12-21 09:59:38 +0300

    pick argv and argc, readme update

---
+++ README.md
@@ -0,0 +1,4 @@
+Author: Jonathan Wambua <jonathanwambua10219@gmail.com>
+
+This package contains posix shell and utilities programs
+implemented in C.
--- uniq/uniq.c
+++ uniq/uniq.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main(int argc, char* argv[])
+{
+    for (int i; i < argc; i++) {
+        printf("\nArgument %d = argv[%d] = %s\n", i, i, argv[i]);
+    }
+    return 0;
+}
